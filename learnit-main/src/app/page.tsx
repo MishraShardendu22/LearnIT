c36c4525d585f14e7f7ce4a5ad6d09803e5c4c58
MishraShardendu22 <shardendumishrabrave@gmail.com>
2025-08-15 23:24:45 +0530

    test
    Signed-off-by: MishraShardendu22 <shardendumishrabrave@gmail.com>

---
+++ learnit-main/src/app/page.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import React, { useEffect } from 'react'
+
+export default function Page() {
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e: Event) => {
+      e.preventDefault()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ;(e as any).prompt()
+    }
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
+    return () => window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
+  }, [])
+
+  return (
+    <div>
+      Hello World 
+    </div>
+  )
+}
